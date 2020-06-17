d7ada2f28b72f6906744ed07343937f0ae462d92
garethx <gareth.wilson@gmail.com>
2020-06-17 14:26:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
     <h2>
-      Start editing to cxvx fsdfsdfsd dfdfds see dfdsfsd somefdfsd fdsfsdfdf
+      Start editing to cxvx fsdfsdfsd dfdfdsdfsdfsdf  see dfdsfsd somefdfsd fdsfsdfdf
       magic happen {"\u2728"}
     </h2>
   </div>
