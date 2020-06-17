6fcdc80626ca1c2bfe3281c9838cf6e08dc9325a
garethx <gareth.wilson@gmail.com>
2020-06-17 14:32:55 +0100

    dfdfsfdfsd

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
     <h2>
-      Start editing 123 to fsdfdf cxvx fsdfsdfsd dfdfdsdfsdfsdf see dfdsfsd
+      Start editing 123 zdsdsto fsdfdf cxvx fsdfsdfsd dfdfdsdfsdfsdf see dfdsfsd
       somefdfsd fdsfsdfdf magic happen {"\u2728"}
     </h2>
   </div>
